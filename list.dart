305b6b8b425abd3f5561af2eb24cbb2db625cbf1
bilal-shiwani <bilalshiwani@gmail.com>
2023-12-05 15:10:09 +0500

    Learn List

---
+++ list.dart
@@ -0,0 +1,13 @@
+void main () {
+print ("ICARS STOCK");
+print ("-----------");
+List<String> cars = ["toyota camry", "toyota revo", "kia sportage", "honda civic",];
+List<int> model = [2020, 2021, 2022, 2023 ];
+print (cars[1]);
+print (model[1]);
+print (cars[3]);
+print (model[3]);
+print (cars[0] + " is sold");
+
+
+}
