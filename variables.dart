9d3928de7b6e4e5be076e3e1cfd6698979518fa1
bilal-shiwani <bilalshiwani@gmail.com>
2023-12-05 14:49:46 +0500

    Learn Variables

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("WELCOME TO ICARS");
+  print("----------------");
+
+  List<String> VEHICALS = ["PRADO", "CIVIC", "REVO", "YARIS"];
+  int total = 4;
+  print("TOTAL CARS IN SHOWROOM=");
+  print(total);
+  print("CARS NAME=");
+  print(VEHICALS);
+  print(VEHICALS[3] + " is sold");
+  bool yarisisnotintheshowroom = true;
+  print("YARIS IS NOT IN THE SHOWROOM");
+  String open = "showroom is open";
+  print(open);
+}
