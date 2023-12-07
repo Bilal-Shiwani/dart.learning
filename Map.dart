86742246ace9062dd08e1e147259d75b2de8c5e3
bilal-shiwani <bilalshiwani@gmail.com>
2023-12-07 14:34:21 +0500

    Learn Map

---
+++ Map.dart
@@ -0,0 +1,27 @@
+void main() {
+  List<Map<String, String>> vehicals = [
+    {
+      " honda civic ": " model 2017 ",
+      " white ":
+  "37000 km full orignal exelent condiotion", 
+    },
+    {
+    "toyota raze ": "model 2019",
+      "black":
+      "1 pc touchup 45000 km exelent condition",
+    },
+    {
+      "toyota vitz ": "model 2020",
+      "grey":
+      "full orignal 4 garde 80000 miliege",
+    },
+    {
+      "honda civic": "model 2021",
+      "black":
+      "full orignal 35000 mileage exelent condition",
+      
+    },
+    
+  ];
+  print(vehicals[3]);
+}
