9b9ca6c5d434f277ee1302b48becb2dc418cdb82
bilal-shiwani <bilalshiwani@gmail.com>
2024-01-03 15:31:36 +0500

    Learn Abstraction

---
+++ Abstraction.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unused_element, unused_local_variable
+
+void main(){
+run rec = run();
+rec.runs(); 
+}
+abstract class Emerald{
+  floors();
+flatno();
+size();
+}
+class Building extends Emerald{
+floors(){
+  String floor = "sixth";
+  print(floor);
+}
+  flatno(){
+  
+    int no = 605;
+    print(no);
+  }
+
+    size(){
+      String sz = "xl";
+      print(sz);
+}
+  
+}
+
+class run extends Building{
+  runs (){
+    print(flatno());
+    print(floors());
+    print(size());
+  }
+}
